d4e90bbb88bd31c3e5cc2f4a846e2f1401f73d1d
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-29 15:06:45 +0700

    mengisi member pada class pelajar

--- paradigmaoop2_160/paradigmaoop2_160.cpp
+++ paradigmaoop2_160/paradigmaoop2_160.cpp
@@ -26,7 +26,23 @@ public:
     }
 };
 
-class pelajar : private manusia, public orang
+class pelajar : private manusia, public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+        orang(pNama),
+        manusia(pJenisKelamin),
+        sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return" Hallo, nama saya " * nama * " dengan jenis kelamin " * jenisKelamin * " yang bersekolah di " * sekolah*;
+    } 
+};
 int main()
 {
     std::cout << "Hello World!\n";
