43492796c7edf74ad2e96c6659337b78381b7908
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-29 15:28:58 +0700

    membuat class budi yang merupakan turunan dari class pekerja dan pelajar

--- problemMultiWaris/problemMultiWaris.cpp
+++ problemMultiWaris/problemMultiWaris.cpp
@@ -32,7 +32,15 @@ public:
     }
 };
 
-
+class budi : public pekerja, public pelajar {
+    public:
+        budi(int pUmur) :
+            pekerja(pUmur),
+            pelajar(pUmur)
+        {
+            cout << "Budi dibuat\n" << endl;
+        }
+};
 
 int main()
 {
