c45aaa2607ebbdf33df8badf1866afeb64e40cb4
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-29 14:47:48 +0700

    Membuat class orang beserta membernya

--- paradigmaoop2_160/paradigmaoop2_160.cpp
+++ paradigmaoop2_160/paradigmaoop2_160.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class orang {
+public:
+    string nama;
+
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "orang dibuat\n" << endl;
+    }
+    ~orang() {
+        cout << "orang dihapus\n" << endl;
+    }
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
